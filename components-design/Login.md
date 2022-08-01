63c1bdabca75c2ae0371adb6aadc39770c578828
chao <chao.c.jiang@outlook.com>
2022-08-01 18:01:20 +0300

    pushed on 22-08-01@18:01:20

---
+++ components-design/Login.md
@@ -0,0 +1,3 @@
+less provide nesting feature to write css
+
+for the Login header, use `display:flex;` and `align-items:center;` to align logo and header text
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,18 @@
 import React, {Component} from "react"
+import "./login.less"
+import logo from "./images/Timma-logos-300.jpeg"
 
 // this is the login router component
 export default class Login extends Component {
     render(){
         return(
-            <div>Login</div>
+            <div className="login">
+                <header className="login-header">
+                    <img src={logo} alt="header-logo"  />
+                    Timma managment system
+                </header>
+                <section className="login-content"></section>
+            </div>
         )
     }
 }
