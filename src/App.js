1cbc319fb97fb22ff6cb31b836fc6e1868f4cbb1
chao <chao.c.jiang@outlook.com>
2022-08-01 11:27:14 +0300

    antd added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { Component } from "react";
 
+import { Button } from 'antd';
+import './App.css';
+
 export default class App extends Component {
   render() {
-    return <div>App from class</div>;
+    return <div>App from class
+      <Button type="primary">Hiiiii</Button>
+    </div>;
   }
 }
