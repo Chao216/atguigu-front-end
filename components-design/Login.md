c526fd06e983f853b691f3a51d3e3c15861e3761
chao <chao.c.jiang@outlook.com>
2022-08-02 18:44:27 +0300

    added rules, and customized validation

--- components-design/Login.md
+++ components-design/Login.md
@@ -1,3 +1,66 @@
 less provide nesting feature to write css
 
 for the Login header, use `display:flex;` and `align-items:center;` to align logo and header text
+
+## verify username and password
+
+### declared validation
+
+rules 是一个数列对象的要求
+
+```javascript
+rules={[
+          {
+            required: true,
+            message: 'Please input your Username!',
+          },
+          {
+            min: 6,
+            message: 'username at least 6 digits',
+          },
+          {
+            max: 12,
+            message: 'username at most 12 digits',
+          },
+          {
+            pattern: /^[a-zA-Z0-9_]+$/,
+            message: 'must contain number, letter, and underscore',
+          },
+        ]}
+```
+
+## customized validation
+
+1. create a validator function on top before render
+use promise now!
+
+```javascript
+ validatePwd = (rule,value)=>{
+    if (!value){
+      return Promise.reject(new Error('cannot be empty'));
+    } else if (value.length<4){
+      return Promise.reject(new Error('at least 4 digits'));
+      
+    } else if (value.length>12){
+      return Promise.reject(new Error('at most 12 digits'));
+      
+    } else if (!/^[a-zA-Z0-9_]+$/.test(value)){
+      return Promise.reject(new Error('must conatin number letter, and underscore'));
+      
+    } else {
+      return Promise.resolve()
+    }
+   }
+```
+
+2. set the rule as validator function
+```javascript
+<Form.Item
+        name="password"
+        rules={[
+          {
+          validator:this.validatePwd
+          }
+        ]}
+      >
+```
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,16 +2,98 @@ import React, {Component} from "react"
 import "./login.less"
 import logo from "./images/Timma-logos-300.jpeg"
 
+// import antd components
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Form, Input } from 'antd';
+
+
 // this is the login router component
 export default class Login extends Component {
+
+   validatePwd = (rule,value)=>{
+    if (!value){
+      return Promise.reject(new Error('cannot be empty'));
+    } else if (value.length<4){
+      return Promise.reject(new Error('at least 4 digits'));
+      
+    } else if (value.length>12){
+      return Promise.reject(new Error('at most 12 digits'));
+      
+    } else if (!/^[a-zA-Z0-9_]+$/.test(value)){
+      return Promise.reject(new Error('must conatin number letter, and underscore'));
+      
+    } else {
+      return Promise.resolve()
+    }
+   }
     render(){
+      const onFinish = (values) => {
+        console.log('Received values of form: ', values);
+      };
         return(
             <div className="login">
                 <header className="login-header">
                     <img src={logo} alt="header-logo"  />
-                    Timma managment system
+                    <h1>TIMMA managment system</h1>
                 </header>
-                <section className="login-content"></section>
+                <section className="login-content">
+                    <h2>Login</h2>
+                    <Form
+      name="normal_login"
+      className="login-form"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your Username!',
+          },
+          {
+            min: 6,
+            message: 'username at least 6 digits',
+          },
+          {
+            max: 12,
+            message: 'username at most 12 digits',
+          },
+          {
+            pattern: /^[a-zA-Z0-9_]+$/,
+            message: 'must contain number, letter, and underscore',
+          },
+        ]}
+      >
+        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[
+          {
+          validator:this.validatePwd
+          }
+        ]}
+      >
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
+      
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Log in
+        </Button>
+        
+      </Form.Item>
+    </Form>
+                    
+                </section>
             </div>
         )
     }
